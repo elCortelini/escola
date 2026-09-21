04eeb64f29ae252542be687b50022bf4e2e09a4e
elCortelini <elcortelini@gmail.com>
2026-09-21 09:34:39 -0300

    chore: remover scripts temporarios de correcao

--- fix_target.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('js/gestao-app.js', 'utf8');
-code = code.replace(/target=["']_blank["']/g, 'target="_blank" rel="noopener noreferrer"');
-code = code.replace(/rel="noopener noreferrer"(\s+)rel="noopener noreferrer"/g, 'rel="noopener noreferrer"');
-fs.writeFileSync('js/gestao-app.js', code, 'utf8');
